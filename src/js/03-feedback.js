fc4198e28d60e872b65875b2ce787fd2d37fac46
Stanislav2022 <tsv5@ukr.net>
2022-08-03 00:09:00 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".feedback-form");
+console.log(form)
+
+form.addEventListener('input', onTextareaInput);
+function onTextareaInput (e) {
+    const message = e.target.value;
+    console.log(message)
+}
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state"
 
