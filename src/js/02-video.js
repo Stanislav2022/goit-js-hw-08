0dc99471c4e8f28f938087a743a943475f3dabe6
Stanislav2022 <tsv5@ukr.net>
2022-08-02 23:30:52 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "videoplayer-current-time"
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player)
+
+player.on('timeupdate', throttle(function({seconds}) {
+    console.log(seconds);
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}, 1000))
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
