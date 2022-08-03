fa4c5e2720b9d7f675eab0c0b4d49fb627c97739
Stanislav2022 <96731862+Stanislav2022@users.noreply.github.com>
2022-08-03 19:07:37 +0000

    Deploying to gh-pages from @ Stanislav2022/goit-js-hw-08@600a4f90cbd1b2b26e95000c825ea6d1eadb6108 🚀

--- 03-feedback.389f8ed3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){var o=e.target.value;console.log(o)}))}();
-//# sourceMappingURL=03-feedback.389f8ed3.js.map
--- 03-feedback.4ec07e0e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){const o=e.target.value;console.log(o)}));
-//# sourceMappingURL=03-feedback.4ec07e0e.js.map
